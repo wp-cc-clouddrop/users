2cdd86a07a3a4912f750266f1ec12bc6cc696c15
pokgak <m.aimanismail@gmail.com>
2019-11-15 14:01:50 +0100

    test remove metrics

--- main.go
+++ main.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"users/internal/api"
-
-	"net/http"
-
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
-	http.Handle("/metrics", promhttp.Handler())
-	go panic(http.ListenAndServe(":8081", nil))
+	// http.Handle("/metrics", promhttp.Handler())
+	// go panic(http.ListenAndServe(":8081", nil))
 
 	api.Init(8080)
 }
